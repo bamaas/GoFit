1d3e3b58d88fd0524ce1059f7e17152113717a69
Bas Maas <hi@basmaas.nl>
2024-03-21 20:54:59 +0100

    Setup logger

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"encoding/json"
-	"strconv"
 	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"strconv"
 )
 
 type entry struct {
-	ID int `json:"id"`
+	ID     int     `json:"id"`
 	Weight float64 `json:"weight"`
 }
 
@@ -20,7 +21,6 @@ var entries = []entry{
 }
 
 func getEntries() []entry {
-	fmt.Println("Getting entries")
 	return entries
 }
 
@@ -33,7 +33,6 @@ func getEntry(id int) (entry, bool) {
 	return entry{}, false
 }
 
-
 // renderJSON renders 'v' as JSON and writes it as a response into w.
 func renderJSON(w http.ResponseWriter, v interface{}) {
 	js, err := json.Marshal(v)
@@ -45,17 +44,39 @@ func renderJSON(w http.ResponseWriter, v interface{}) {
 	w.Write(js)
 }
 
-func main() {
-	mux := http.NewServeMux()
+var logLevel map[string]slog.Level = map[string]slog.Level{
+	"DEBUG": slog.LevelDebug,
+	"INFO":  slog.LevelInfo,
+	"WARN":  slog.LevelWarn,
+	"ERROR": slog.LevelError,
+}
 
+func main() {
+
+	// Setup logger
+	level := logLevel["INFO"]
+	envLogLevel := os.Getenv("LOGLEVEL")
+	if envLogLevel != "" {
+		_, ok := logLevel[envLogLevel]
+		if ok {
+			level = logLevel[envLogLevel]
+		} else {
+			slog.Info("Invalid log level, falling back to default", "level", level)
+		}
+	}
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: level}))
+
+	// Setup mux & routes
+	mux := http.NewServeMux()
 	mux.HandleFunc("GET /entries/", func(w http.ResponseWriter, r *http.Request) {
+		logger.Info("Getting entries")
 		entries = getEntries()
 		renderJSON(w, entries)
 	})
 
 	mux.HandleFunc("GET /entry/{id}/", func(w http.ResponseWriter, r *http.Request) {
 		id := r.PathValue("id")
-		fmt.Fprintf(w, "handling entry with id=%v\n", id)
+		logger.Info("Getting entry", "id", id)
 		idInt, err := strconv.Atoi(id)
 		if err != nil {
 			http.Error(w, "invalid id", http.StatusInternalServerError)
@@ -70,18 +91,37 @@ func main() {
 	})
 
 	mux.HandleFunc("POST /entry/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "handling POST entry\n")
+		logger.Info("Creating entry")
 		body, err := io.ReadAll(r.Body)
 		var e entry
 		if err != nil {
 			http.Error(w, "error reading body", http.StatusInternalServerError)
 		}
 		err = json.Unmarshal(body, &e)
+		logger.Debug("Creating entry", "entry", e)
 		if err != nil {
 			http.Error(w, "error parsing body", http.StatusInternalServerError)
 		}
 		entries = append(entries, e)
 	})
 
+	mux.HandleFunc("DELETE /entry/{id}/", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		logger.Info("Deleting entry", "id", id)
+		idInt, err := strconv.Atoi(id)
+		if err != nil {
+			http.Error(w, "invalid id", http.StatusInternalServerError)
+			return
+		}
+		for i, v := range entries {
+			if v.ID == idInt {
+				entries = append(entries[:i], entries[i+1:]...)
+				return
+			}
+		}
+		http.Error(w, "entry not found", http.StatusNotFound)
+	})
+
+	logger.Info("Starting server")
 	http.ListenAndServe("localhost:8080", mux)
-  }
+}
