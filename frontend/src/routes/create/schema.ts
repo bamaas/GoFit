c9a274dd4555b7e7ceb93b26a77b5e8909c7f5fc
Bas Maas <beheerder@MacBook-Pro-van-Jeffrey.local>
2024-03-30 21:56:43 +0100

    datepicker

--- frontend/src/routes/create/schema.ts
+++ frontend/src/routes/create/schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-    date: z.coerce.date().default(new Date()),
+    date: z.string(),
     weight: z.coerce.number().min(20).max(200).default(0),
 });
  
