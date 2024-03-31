7293a1d462f4c65d3f67ca0fe0c907269163c1e8
Bas Maas <beheerder@MacBook-Pro-van-Jeffrey.local>
2024-03-31 22:50:00 +0200

    added alert dialog to delete operation on frontend

--- frontend/src/routes/logbook/[uuid]/delete-button.svelte
+++ frontend/src/routes/logbook/[uuid]/delete-button.svelte
@@ -5,6 +5,7 @@
     import { page } from '$app/stores';
 	import { toast } from "svelte-sonner";
 	import { goto } from "$app/navigation";
+    import * as AlertDialog from "$lib/components/ui/alert-dialog";
 
     let uuid = "";
     $: uuid = $page.params.uuid;
@@ -30,7 +31,24 @@
       }
 
 </script>
+
+<AlertDialog.Root>
+    <AlertDialog.Trigger>
+        <Button variant="destructive">
+            <Trash2Icon class="size-3.5"/>
+        </Button>
+    </AlertDialog.Trigger>
+    <AlertDialog.Content>
+      <AlertDialog.Header>
+        <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+        <AlertDialog.Description>
+          This action cannot be undone. This will permanently delete your check-in from our servers.
+        </AlertDialog.Description>
+      </AlertDialog.Header>
+      <AlertDialog.Footer>
+        <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+        <AlertDialog.Action on:click={deleteCheckIn}>Delete</AlertDialog.Action>
+      </AlertDialog.Footer>
+    </AlertDialog.Content>
+</AlertDialog.Root>
    
-<Button variant="destructive" on:click={deleteCheckIn}>
-    <Trash2Icon class="size-3.5"/>
-</Button>
