b790fd11c39bb177766c1cc8f219b193f0e7708e
Bas Maas <beheerder@MacBook-Pro-van-Jeffrey.local>
2024-04-18 19:42:04 +0200

    show minus icon when there is no weight diff

--- frontend/src/routes/logbook/weight-diff-column.svelte
+++ frontend/src/routes/logbook/weight-diff-column.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
     import MoveUpIcon from "lucide-svelte/icons/move-up";
     import MoveDownIcon from "lucide-svelte/icons/move-down";
+    import MinusIcon from "lucide-svelte/icons/minus";
     export let weightDiff: number;
 </script>
 <div class="flex flex-row-reverse">
-    <span>{Math.abs(weightDiff)} kg</span>
+    {#if weightDiff != 0}
+        <span>{Math.abs(weightDiff)} kg</span>
+    {/if}
     {#if weightDiff > 0}
         <MoveUpIcon class="h-4 w-4"/>
     {:else if weightDiff < 0}
         <MoveDownIcon class="h-4 w-4"/>
+    {:else}
+        <MinusIcon class="h-4 w-4"/>
     {/if}
 </div>
