176cb7c35954e4852253538acb32d93d568e98de
Bas Maas <beheerder@MacBook-Pro-van-Jeffrey.local>
2024-04-23 16:53:49 +0200

    Added avatar to header

--- frontend/src/routes/header.svelte
+++ frontend/src/routes/header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import { CircleUserRoundIcon } from "lucide-svelte"
+    import { UserRoundIcon } from "lucide-svelte"
+    import * as Avatar from "$lib/components/ui/avatar/index.js";
 
     export let title: string = "GoFit";
 </script>
@@ -7,7 +8,13 @@
     <div class="flex container h-14 max-w-screen-2xl items-center justify-between">
         <h1 class="croll-m-20 text-l font-semibold tracking-tight lg:text-l">{title}</h1>
         <div class="flex items-center gap-4 text-sm lg:gap-6">
-            <CircleUserRoundIcon class="size-5 transition-colors hover:text-foreground/80 text-foreground/60"/>
+            <Avatar.Root class="items-center h-8 w-8 border">
+                <Avatar.Image src="https://avatars.githubusercontent.com/u/59253720?s=400&u=6f8fcb11a70d4fe4c3a6aa8a4a7c7f0d8772d4e0&v=4" alt="@shadcn" />
+                <Avatar.Fallback>
+                    <UserRoundIcon class="size-4"/>
+                </Avatar.Fallback>
+              </Avatar.Root>
+            
         </div>
     </div>
 </header>
